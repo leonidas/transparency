9f8b2724ba9162bbd95643ec745d82010011dc9b
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-07 14:41:43 +0300

    Compile support for nested models

--- jquery.transparency.js
+++ jquery.transparency.js
@@ -11,14 +11,18 @@
       tmp = template.clone();
       jQuery.each(object, function(key, value) {
         var attribute, klass, _ref;
-        _ref = key.split('@'), klass = _ref[0], attribute = _ref[1];
-        return tmp.find("." + klass).each(function() {
-          if (attribute) {
-            return jQuery(this).attr(attribute, value);
-          } else {
-            return jQuery(this).prepend(value);
-          }
-        });
+        if (jQuery.isArray(value)) {
+          return tmp.find("." + key).children().first().render(value);
+        } else {
+          _ref = key.split('@'), klass = _ref[0], attribute = _ref[1];
+          return tmp.find("." + klass).each(function() {
+            if (attribute) {
+              return jQuery(this).attr(attribute, value);
+            } else {
+              return jQuery(this).prepend(value);
+            }
+          });
+        }
       });
       return context.before(tmp);
     });
