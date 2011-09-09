6e43bcb958d1dc95037050d933d612ce917088a2
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-09 07:51:52 +0300

    Compile new versions

--- jquery.transparency.js
+++ jquery.transparency.js
@@ -1,5 +1,5 @@
 (function() {
-  var $, assign;
+  var $, assign, select;
   $ = jQuery;
   assign = function(node, attribute, value) {
     var children;
@@ -11,34 +11,56 @@
       return node.append(children);
     }
   };
+  select = function(object, fn) {
+    var key, result, value;
+    result = {};
+    for (key in object) {
+      value = object[key];
+      if (fn(key, value)) {
+        result[key] = value;
+      }
+    }
+    return result;
+  };
   $.fn.render = function(data) {
-    var context, template;
+    var attribute, context, key, klass, lists, object, objects, result, template, value, values, _i, _len, _ref;
     if (!$.isArray(data)) {
       data = [data];
     }
     context = this;
     template = this.clone();
-    $.each(data, function(index, object) {
-      var tmp;
-      tmp = template.clone();
-      $.each(object, function(key, value) {
-        var attribute, klass, _ref;
-        if ($.isArray(value)) {
-          return tmp.find("." + key).children().first().render(value);
-        } else if (typeof value === "object") {
-          return tmp.find("." + key).render(value);
-        } else {
-          _ref = key.split('@'), klass = _ref[0], attribute = _ref[1];
-          if (tmp.hasClass(klass)) {
-            assign(tmp, attribute, value);
-          }
-          return tmp.find("." + klass).each(function() {
-            return assign($(this), attribute, value);
-          });
-        }
+    for (_i = 0, _len = data.length; _i < _len; _i++) {
+      object = data[_i];
+      values = select(object, function(key, value) {
+        return typeof value === "string";
       });
-      return context.before(tmp);
-    });
+      objects = select(object, function(key, value) {
+        return typeof value === "object" && !$.isArray(value);
+      });
+      lists = select(object, function(key, value) {
+        return $.isArray(value);
+      });
+      result = template.clone();
+      for (key in values) {
+        value = values[key];
+        _ref = key.split('@'), klass = _ref[0], attribute = _ref[1];
+        if (result.hasClass(klass)) {
+          assign(result, attribute, value);
+        }
+        result.find("." + klass).each(function() {
+          return assign($(this), attribute, value);
+        });
+      }
+      for (key in lists) {
+        value = lists[key];
+        result.find("." + key).children().first().render(value);
+      }
+      for (key in objects) {
+        value = objects[key];
+        result.find("." + key).render(value);
+      }
+      context.before(result);
+    }
     return context.remove();
   };
 }).call(this);
