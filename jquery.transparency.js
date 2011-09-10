1108b14a891b51e7b1b5108af14c26d35a5606c0
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-10 14:17:07 +0300

    Compile new version

--- jquery.transparency.js
+++ jquery.transparency.js
@@ -1,15 +1,42 @@
 (function() {
-  var $, assign, select;
-  $ = jQuery;
-  assign = function(node, attribute, value) {
+  var assignValue, renderKey, select, validAttribute;
+  renderKey = function(key, value, buffer) {
+    var attribute, element, klass, _, _ref, _ref2;
+    _ref2 = (_ref = key.split('@'), element = _ref[0], _ = _ref[1], _ref), klass = _ref2[0], attribute = _ref2[1];
+    if (buffer.hasClass(klass || buffer.is(element))) {
+      assignValue(buffer, attribute, value);
+    }
+    return buffer.find("" + element + ", ." + klass).each(function() {
+      return assignValue(jQuery(this), attribute, value);
+    });
+  };
+  assignValue = function(node, attribute, value) {
     var children;
     if (attribute) {
+      if (!validAttribute(attribute)) {
+        throw "" + attribute + ": Unsafe attribute assignment";
+      }
       return node.attr(attribute, value);
     } else {
       children = node.children().detach();
       node.text(value);
       return node.append(children);
     }
+  };
+  validAttribute = function(attribute) {
+    var valid, valids;
+    valids = [/^src$/, /^alt$/, 'id', /^href$/, /^class$/, /^data-*/];
+    return ((function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = valids.length; _i < _len; _i++) {
+        valid = valids[_i];
+        if (attribute.match(valid)) {
+          _results.push(true);
+        }
+      }
+      return _results;
+    })()).length === 1;
   };
   select = function(object, fn) {
     var key, result, value;
@@ -22,37 +49,42 @@
     }
     return result;
   };
-  $.fn.render = function(data) {
-    var attribute, context, key, klass, object, objects, result, template, value, values, _i, _len, _ref;
-    context = $.isArray(data) ? this.children().first() : this;
+  jQuery.fn.render = function(data, directives) {
+    var buffer, context, directive, key, klass, local_directives, local_values, object, objects, template, value, _i, _len;
+    if (directives == null) {
+      directives = {};
+    }
+    context = jQuery.isArray(data) ? this.children().first() : this;
     template = context.clone();
-    if (!$.isArray(data)) {
+    if (!jQuery.isArray(data)) {
       data = [data];
     }
     for (_i = 0, _len = data.length; _i < _len; _i++) {
       object = data[_i];
-      values = select(object, function(key, value) {
-        return typeof value === "string";
+      local_values = select(object, function(key, value) {
+        return typeof value === 'string';
+      });
+      local_directives = select(directives, function(key, directive) {
+        return typeof directive === 'function';
       });
       objects = select(object, function(key, value) {
-        return typeof value === "object";
+        return typeof value === 'object';
       });
-      result = template.clone();
-      for (key in values) {
-        value = values[key];
-        _ref = key.split('@'), klass = _ref[0], attribute = _ref[1];
-        if (result.hasClass(klass)) {
-          assign(result, attribute, value);
-        }
-        result.find("." + klass).each(function() {
-          return assign($(this), attribute, value);
-        });
+      buffer = template.clone();
+      for (key in local_values) {
+        value = local_values[key];
+        renderKey(key, value, buffer);
       }
-      for (key in objects) {
-        value = objects[key];
-        result.find("." + key).render(value);
+      for (key in local_directives) {
+        directive = local_directives[key];
+        value = directive.call(object);
+        renderKey(key, value, buffer);
       }
-      context.before(result);
+      for (klass in objects) {
+        value = objects[klass];
+        buffer.find("." + klass).add(key).render(value, directives[klass]);
+      }
+      context.before(buffer);
     }
     return context.remove();
   };
