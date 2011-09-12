cb42623cf6e1c8d88cfb265ad9895abba1a3d774
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-12 16:52:06 +0300

    cleanup

--- examples/blog/public/javascripts/application.coffee
+++ examples/blog/public/javascripts/application.coffee
@@ -1,26 +1,21 @@
-showArticle = (article) ->
-  $('.content').empty().append $('.template .show_article.page').clone()
-  $('.content .article').render article
-
 $ ->
   Spine.Route.add
     '/': () ->
-      console.log '/'
       $('.content').empty().append $('.template .index.page').clone()
       $.getJSON '/articles.json', (articles) ->
         $('.content .articles').render articles, 'title@href': () -> ("/articles/#{this.id}")
 
     '/articles/:id': (params) ->
-      console.log params.id
-      $.getJSON "/articles/#{params.id}.json", showArticle
+      $.getJSON "/articles/#{params.id}.json", (article) ->
+        $('.content').empty().append $('.template .show_article.page').clone()
+        $('.content .article').render article
 
     '/articles/new': () ->
-      console.log '/articles/new'
       $('.content').empty().append $('.template .new_article.page').clone()
       form = $('.content .new_article form')
       form.submit (event) ->
         event.preventDefault()
-        $.post form.attr("action"), form.serialize(), (article) ->
-          Spine.Route.navigate("/articles", article.id, true)
+        $.post form.attr("action"), form.serialize(), (article_path) ->
+          Spine.Route.navigate(article_path, true)
 
   Spine.Route.setup(history: true)
--- examples/blog/public/javascripts/application.js
+++ examples/blog/public/javascripts/application.js
@@ -1,13 +1,7 @@
 (function() {
-  var showArticle;
-  showArticle = function(article) {
-    $('.content').empty().append($('.template .show_article.page').clone());
-    return $('.content .article').render(article);
-  };
   $(function() {
     Spine.Route.add({
       '/': function() {
-        console.log('/');
         $('.content').empty().append($('.template .index.page').clone());
         return $.getJSON('/articles.json', function(articles) {
           return $('.content .articles').render(articles, {
@@ -18,18 +12,19 @@
         });
       },
       '/articles/:id': function(params) {
-        console.log(params.id);
-        return $.getJSON("/articles/" + params.id + ".json", showArticle);
+        return $.getJSON("/articles/" + params.id + ".json", function(article) {
+          $('.content').empty().append($('.template .show_article.page').clone());
+          return $('.content .article').render(article);
+        });
       },
       '/articles/new': function() {
         var form;
-        console.log('/articles/new');
         $('.content').empty().append($('.template .new_article.page').clone());
         form = $('.content .new_article form');
         return form.submit(function(event) {
           event.preventDefault();
-          return $.post(form.attr("action"), form.serialize(), function(article) {
-            return Spine.Route.navigate("/articles", article.id, true);
+          return $.post(form.attr("action"), form.serialize(), function(article_path) {
+            return Spine.Route.navigate(article_path, true);
           });
         });
       }
