1e1b273c87a2a2db2a51fbe3e86c31607e2d48a3
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2012-10-28 21:28:43 +0200

    Update Backbone example

--- examples/backbone/js/app.js
+++ examples/backbone/js/app.js
@@ -1,83 +1,53 @@
 $(function() {
   var CustomersView, CustomerRowView, CustomerModel, CustomerCollection;
 
-  CustomerModel = Backbone.Model.extend({
-
-  });
+  CustomerModel = Backbone.Model.extend({});
 
   CustomerCollection = Backbone.Collection.extend({
     model: CustomerModel,
     url: 'customers.json'
   });
 
-  CustomerRowView = Backbone.View.extend({
-    tagName: 'tr',
-    events: {
-      'click .delete': 'deleteModel'
-    },
-    templateBase: $('#templates .tmpl-customerrow').detach(),
-    initialize: function() {
-      this.template = this.templateBase.clone();
-    },
-    render: function() {
-      data = this.model.toJSON();
-      this.$el.html(this.template.render(data).children());
-      return this;
-    },
-    deleteModel: function() {
-      //Here you would actually call this.model.destroy() and you would
-      //have a callback for the "destroy" event
-      alert('Removing');
-      return false;
-    }
-  });
-
   CustomersView = Backbone.View.extend({
     events: {
-      'submit .new-customer': 'addNewCustomer'
+      'submit .new-customer': 'addCustomer',
+      'click .delete': 'deleteCustomer'
     },
+
     el: $('#application'),
-    templateBase: $('#templates > .tmpl-customers').detach(),
+
     render: function() {
-      var data = {
-        suffixes: ['Ms.', 'Mrs.', 'Mr.']
-      }, directives = {
-        suffixes: {
-          suffix: {
-            html: function() {
-              return this.value;
-            }
-          }
+      this.$('.customers').render(customers.toJSON(), {
+        delete: {
+          index: function(params) { return params.index; }
         }
-      };
-      this.$el.html(this.template.render(data, directives))
+      });
+
       return this;
     },
+
     initialize: function() {
-      this.template = this.templateBase.clone();
-      this.customers = new CustomerCollection();
-      this.customers.on('add', this.addOne, this);
-      this.customers.on('reset', this.addAll, this);
+      window.customers = this.customers = new CustomerCollection();
+      this.customers.on('add', this.render, this);
+      this.customers.on('reset', this.render, this);
+      this.customers.on('destroy', this.render, this);
       this.customers.fetch();
     },
-    addNewCustomer: function() {
+
+    addCustomer: function() {
       this.customers.create({
         suffix: this.$('.new-customer .suffixes').val(),
-        name: this.$('.new-customer .name').val(),
-        email: this.$('.new-customer .email').val(),
-        phone: this.$('.new-customer .phone').val()
+        name:   this.$('.new-customer .name').val(),
+        email:  this.$('.new-customer .email').val(),
+        phone:  this.$('.new-customer .phone').val()
       });
 
       return false;
     },
-    addOne: function(model) {
-      var view = new CustomerRowView({
-        model: model
-      });
-      this.$('.customers').append(view.render().el);
-    },
-    addAll: function() {
-      this.customers.forEach(this.addOne);
+
+    deleteCustomer: function(event) {
+      customers.at(event.target.getAttribute('index')).destroy();
+      return false;
     }
   });
 
