cf1e609a4992caa03964e2fb646bde7851b0f85c
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2013-05-08 11:01:15 +0300

    Get rid of lodash, use trivial implementations instead.

--- examples/hello-server/server.js
+++ examples/hello-server/server.js
@@ -1,8 +1,8 @@
-var express      = require("express"),
-    jsdom        = require("jsdom").jsdom,
-    $            = require("jquery"),
-    Transparency = require("../../index"),
-    helloTmpl    = $('<ul id="items"><li class="title"></li></ul>')[0],
+var express      = require('express'),
+    jsdom        = require('jsdom').jsdom,
+    doc          = jsdom('<html><body><ul id="items"><li class="title"></li></ul></body></html>'),
+    Transparency = require('../../index'),
+    helloTmpl    = doc.getElementsByTagName('ul')[0],
     count        = 0,
     app;
 
@@ -10,19 +10,19 @@ app = express();
 
 app.configure(function() { app.use(app.router); });
 
-app.configure("development", function() {
+app.configure('development', function() {
   app.use(express.errorHandler({
     dumpExceptions: true,
     showStack: true
   }));
 });
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   var data = [
-    { title: "Hello "  + count++},
-    { title: "Howdy "  + count++},
-    { title: "Cheers " + count++},
-    { title: "Byebye " + count++}
+    { title: 'Hello '  + count++},
+    { title: 'Howdy '  + count++},
+    { title: 'Cheers ' + count++},
+    { title: 'Byebye ' + count++}
   ];
 
   return res.send(Transparency.render(helloTmpl, data).outerHTML);
@@ -30,4 +30,4 @@ app.get("/", function(req, res) {
 
 app.listen(3000);
 
-console.log("Express server listening on port 3000 in %s mode", app.settings.env);
+console.log('Express server listening on port 3000 in %s mode', app.settings.env);
